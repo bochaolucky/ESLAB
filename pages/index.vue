1b8cd55876fc7d377d2be19062d31e0a0df76f9a
bochao <bochaolucky@gmail.com>
2024-04-14 23:06:12 +0800

    tyuiiu

--- pages/index.vue
+++ pages/index.vue
@@ -31,7 +31,7 @@
               <p>
                最近转向自动驾驶和实时机器人操作系统（ROS），以及无电池能量采集系统。在与我的研究合作伙伴的合作下，我们深入研究了操作系统和运行时、间歇计算和值得信赖的实时边缘人工智能，特别关注自动驾驶。有关我们最近的努力，请访问ESL研究小组。
               </p>
-              
+
               <h4>机器人操作系统</h4>
               <p>
                最近转向自动驾驶和实时机器人操作系统（ROS），以及无电池能量采集系统。在与我的研究合作伙伴的合作下，我们深入研究了操作系统和运行时、间歇计算和值得信赖的实时边缘人工智能，特别关注自动驾驶。有关我们最近的努力，请访问ESL研究小组。
@@ -41,6 +41,41 @@
               <p>
                最近转向自动驾驶和实时机器人操作系统（ROS），以及无电池能量采集系统。在与我的研究合作伙伴的合作下，我们深入研究了操作系统和运行时、间歇计算和值得信赖的实时边缘人工智能，特别关注自动驾驶。有关我们最近的努力，请访问ESL研究小组。
               </p>
+            </li>
+
+          </ul>
+
+          <h1>科研项目</h1>
+          <ul>
+            <li>
+              <h4>Intermittent Computing Systems（视频啥的以后换成咱们自己的）</h4>
+              <p>
+               最近转向自动驾驶和实时机器人操作系统（ROS），以及无电池能量采集系统。在与我的研究合作伙伴的合作下，我们深入研究了操作系统和运行时、间歇计算和值得信赖的实时边缘人工智能，特别关注自动驾驶。有关我们最近的努力，请访问ESL研究小组。（内容与实际无关）
+              </p>
+
+                  <p style="display: flex;align-items: center;margin-top: 20px">查看论文：
+                    <el-link href="https://arxiv.org/abs/2002.11711" target="_blank">https://arxiv.org/abs/2002.11711</el-link>
+                  </p>
+                  <p style="display: flex;align-items: center;margin-top: 10px">在线实验：
+                    <el-link href="http://demo.blockchain-neu.com" target="_blank">http://demo.blockchain-neu.com</el-link>
+                  </p>
+
+                  <p style="display: flex;align-items: center;margin-top: 10px">视频演示：</p>
+
+                  <div style="width: 100%;text-align: center">
+                    <video width="640" height="360" controls="controls" preload="metadata">
+                    <source :src="'/FedCoin.mp4'" type="video/mp4">
+                    </video>
+                  </div>
+
+                  <p style="display: flex;align-items: center;margin-top: 20px">备用播放地址1（以后换成咱们自己的）：
+                    <el-link href="https://v.youku.com/v_show/id_XNDU2NjA5NzQ3Mg==.html" target="_blank">优酷</el-link>
+                  </p>
+                    <p style="display: flex;align-items: center;margin-top: 10px">备用播放地址2：
+                    <el-link href="https://youtu.be/u5LPLdZvd0g" target="_blank">YouTube</el-link>
+                  </p>
+
+                  <br>
             </li>
 
           </ul>
