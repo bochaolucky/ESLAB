337e47fa066373f9badff918ea9d5ee4dabcdfde
bochao <bochaolucky@gmail.com>
2024-04-14 18:42:26 +0800

    rrrrw

--- components/myheader.vue
+++ components/myheader.vue
@@ -7,7 +7,7 @@
         <div class="content">
           <el-row :gutter="0" type="flex" justify="center">
 
-            <el-col :xs="11" :sm="10" :md="9" :lg="8" :xl="7">
+            <el-col :xs="9" :sm="9" :md="9" :lg="9" :xl="9">
               <div v-for="item in titles1"
                    v-bind:class="{'selected':index===item.key,'mousein':mouseindex===item.key}"
                    @mouseenter="mouseenter(item.key)" @mouseleave="mouseleave()" class="title-item">
